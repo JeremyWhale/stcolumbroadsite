dce7d8d6ba7fff55e511023f1464afe3315b94f7
JeremyWhale <jeremywhale@gmail.com>
2024-06-16 20:14:03 +0100

    units format

--- stcolumbfrontend/src/routes/Units.svelte
+++ stcolumbfrontend/src/routes/Units.svelte
@@ -6,7 +6,7 @@
     <h1>Units to Rent</h1>
     <p>Periodically, units at this Industrial Estate in St Columb Road come available for rent. </p>
     <p>This page will list those units so keep checking back for the latest availability.</p>
-    <div>
+    <div class="container">
         <h2>Current Availability</h2>
         <p>There are currently no units available to rent.</p>
         <p>For more information, please contact us on 01726 73173 or 07733 534 438. or via email at whalecommercial@gmail.com</p>
@@ -17,5 +17,9 @@
 </main>
 
 <style>
-    
+    .container {
+        background-color: #cccccc;;
+        border-radius: 10px;
+        padding: 10px;
+    }
 </style>
