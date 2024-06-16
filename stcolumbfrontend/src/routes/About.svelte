5f1eed085a6eac9ca3f55385ebd2a146a73645f3
JeremyWhale <jeremywhale@gmail.com>
2024-06-16 20:15:58 +0100

    about content

---
+++ stcolumbfrontend/src/routes/About.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+
+</script>
+
+<template>
+    <h1>About Page</h1>
+    <p>
+        St Columb Road Workshop Units, is a private commercial estate, ideally suited to small to medium sized businesses. We have 5 units of various sizes which are suitable for warehouse / storage facilities, work-unit, internet-based businesses etc.
+    </p>
+    <p>
+        This site is created and managed by Jeremy Whale. Email: jeremywhale@gmail.com and is an SPA made with Svelte hosted on AWS Amplify.
+    </p>
+</template>
+
+<style>
+
+</style>
