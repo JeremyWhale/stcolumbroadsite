f3af0d4e578bfc3f64baede4aaaeeadf0c56b06e
JeremyWhale <jeremywhale@gmail.com>
2024-07-07 14:12:27 +0100

    Remove unused components from main

--- stcolumbfrontend/src/routes/Units.svelte
+++ stcolumbfrontend/src/routes/Units.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-    import UnitSlideshow from "../lib/UnitSlideshow.svelte";
-</script>
-
 <main>
     <h1>Units to Rent</h1>
     <p>Periodically, units at this Industrial Estate in St Columb Road come available for rent. </p>
@@ -13,7 +9,6 @@
         <p>There is an additional service charge amount to cover Buildings Insurance currently at £560 + VAT per annum. The service charge is reviewed annually on the 1st of December.</p>             
         <p>Tenants are responsible for the payment of non-domestic rates on the premises; many occupiers will benefit from small business relief. This information can be obtained from Cornwall Council.</p>
         <p class="disclaimer">**Subject to Contract**</p>
-        <!-- <UnitSlideshow/>-->
         <img src="/IMG_3534.jpg" alt="Image of Site St Columb Road Industrial Units for let" title="Unit-1E-StColumbRoad-Industrial-Units-For-Rent">
         <img src="/IMG_3527.jpg" alt="Image of Site St Columb Road Industrial Units for let" title="Unit-1E-StColumbRoad-Industrial-Units-For-Rent">
         <p>For more information, please contact us on 01726 73173 or 07733 534 438. or via email at whalecommercial@gmail.com</p>
